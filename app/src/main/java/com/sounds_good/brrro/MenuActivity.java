b716fe1e39bf3f3bc3545da12e092a9730e6a654
pgoggijr <petergoggijr@gmail.com>
2015-04-23 14:23:31 -0400

    ctivities now use non-deprecated AppCompatActivity

--- app/src/main/java/com/sounds_good/brrro/MenuActivity.java
+++ app/src/main/java/com/sounds_good/brrro/MenuActivity.java
@@ -1,14 +1,14 @@
 package com.sounds_good.brrro;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 
 
-public class MenuActivity extends ActionBarActivity {
+public class MenuActivity extends AppCompatActivity {
 
     public void checkHistory(View view) {
         System.out.println("checking history");
--- app/src/main/java/com/sounds_good/brrro/WorkoutActivity.java
+++ app/src/main/java/com/sounds_good/brrro/WorkoutActivity.java
@@ -1,12 +1,12 @@
 package com.sounds_good.brrro;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class WorkoutActivity extends ActionBarActivity {
+public class WorkoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
