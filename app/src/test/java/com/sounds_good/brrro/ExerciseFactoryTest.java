3737a6a4a09717d25667cffd6ed1d3f2f09bfcf6
Matt Telker <matttelker@gmail.com>
2015-05-06 23:21:26 -0400

    added unit tests

--- app/src/test/java/com/sounds_good/brrro/ExerciseFactoryTest.java
+++ app/src/test/java/com/sounds_good/brrro/ExerciseFactoryTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
+/*
  * Created by Peter on 5/6/2015.
  */
 public class ExerciseFactoryTest {
@@ -16,15 +16,29 @@ public class ExerciseFactoryTest {
     private Exercise testExercise3;
     private Exercise testExercise4;
     private Exercise testExercise5;
+    private Exercise testExercise6;
+    private Exercise testExercise7;
+    private Exercise testExercise8;
+    private Exercise testExercise9;
+
+    /*int[] arr = new int[0];
+    for (int i = 0; i < 5; i++){
+        arr[i] = 0;
+    }*/
 
     @Before
     public void setUp() throws Exception {
+        int[] arr = {0,0,0,0,0};
         testFactory = new ExerciseFactory();
         testExercise1 = new Exercise(5, 5, 45, ExerciseFactory.SQUAT);
         testExercise2 = new Exercise(3, 8, 45, ExerciseFactory.BARBELL_SHRUGS);
         testExercise3 = new Exercise(2, 10, 45, ExerciseFactory.HYPEREXTENSIONS);
         testExercise4 = new Exercise(1, 5, 45, ExerciseFactory.DEADLIFT);
         testExercise5 = new Exercise(3, 10, 45, ExerciseFactory.CABLE_CRUNCHES);
+        testExercise6 = new Exercise(2, 10, 45, ExerciseFactory.HYPEREXTENSIONS,arr);
+        testExercise7 = new Exercise(1, 5, 45, ExerciseFactory.DEADLIFT,arr);
+        testExercise8 = new Exercise(3, 8, 45, ExerciseFactory.CLOSE_GRIP_BENCH_PRESS,arr);
+        testExercise9 = new Exercise(0 ,0 ,0 ,100 , arr);
     }
 
     @Test
@@ -51,6 +65,14 @@ public class ExerciseFactoryTest {
 
     @Test
     public void testCreateExercise1() throws Exception {
-
+        int[] arr1 = {0,0,0,0,0};
+        assertTrue("creating hyperextension should be equal to testExercise6",
+                testFactory.createExercise(ExerciseFactory.HYPEREXTENSIONS,45,arr1).equalTo(testExercise6));
+        assertTrue("creating deadlift should be equal to testExercise7",
+                testFactory.createExercise(ExerciseFactory.DEADLIFT, 45, arr1).equalTo(testExercise7));
+        assertTrue("creating close grip bench press should be equal to testExercise8",
+                testFactory.createExercise(ExerciseFactory.CLOSE_GRIP_BENCH_PRESS, 45, arr1).equalTo(testExercise8));
+        assertTrue("creating bogus exercise should be equal to testExercise9",
+                testFactory.createExercise(100,0,arr1).equalTo(testExercise9));
     }
 }
